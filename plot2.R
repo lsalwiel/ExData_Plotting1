e9b6b74750c6f1a1045bf8915c9c6984302794db
Leslie Alwiel <leslie.alwiel@gmail.com>
2017-03-30 10:13:00 -0700

    Add files via upload
    files for plot2 assignment

---
+++ plot2.R
@@ -0,0 +1,14 @@
+##Data set is large and assumed to be on local computer
+##Read Data and subset for plot 2
+hpc <- read.csv("household_power_consumption.txt", sep=";", header=TRUE,stringsAsFactors=FALSE,na.strings="?")
+hpc1 <- hpc[(hpc$Date=="1/2/2007" | hpc$Date=="2/2/2007" ), ]
+
+#formatting for time and date
+hpc1 <- filter(hpc,Date %in% c("1/2/2007","2/2/2007")) 
+hpc1$DateTime <- strptime(paste(hpc1$Date,hpc1$Time),"%d/%m/%Y %H:%M:%S")
+
+##plot and create png file
+png("plot2.png",width=480,height=480)
+plot(hpc1$DateTime, hpc1$Global_active_power,type="l",ylab="Global Active Power (kilowatts)",xlab=NA)
+dev.off()
+
